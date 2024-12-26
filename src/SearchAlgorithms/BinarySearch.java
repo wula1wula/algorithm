b3ff929dafc47a3649e1242486c310dffcbb8931
wula <3070510372@qq.com>
2024-12-26 09:53:23 +0800

    二分搜索

--- src/SearchAlgorithms/BinarySearch.java
+++ src/SearchAlgorithms/BinarySearch.java
@@ -1,7 +1,11 @@
 package SearchAlgorithms;
 
 public class BinarySearch {
+    //递归
     public int binarySearch(int[] array, int target) {
+        if(array == null || array.length == 0){
+            return -1;
+        }
         int left = 0;
         int right = array.length - 1;
         return binarySearch(array, target, left, right);
@@ -10,7 +14,7 @@ public class BinarySearch {
         if(left > right) {
             return -1;
         }
-        int mid = (left + right) / 2;
+        int mid = left+(right-left) / 2;
         if(array[mid]==target){
             return mid;
         }
@@ -19,11 +23,32 @@ public class BinarySearch {
         }
         return binarySearch(array, target, mid + 1, right);
     }
+    //循环
+    public int binarySearchIterative(int[] array, int target) {
+        if(array == null || array.length == 0){
+            return -1;
+        }
+        int left =0;
+        int right = array.length - 1;
+        while(left <= right) {
+            int mid = left+(right-left) / 2;
+            if(array[mid]==target){
+                return mid;
+            }
+            if(array[mid] > target) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int target = 5;
         int index = binarySearch.binarySearch(array, target);
-        System.out.println("The index of " + target + " is " + index);
+        int index_2 = binarySearch.binarySearchIterative(array,target);
+        System.out.println("The index of " + target + " is " + index+"or"+index_2);
     }
 }
