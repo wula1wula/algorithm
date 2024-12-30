7cb903758f8c73dd7188997d8d4296be0c03a416
wula <3070510372@qq.com>
2024-12-30 13:35:59 +0800

    邻接列表广度优先

---
+++ src/SearchAlgorithms/ListBFS.java
@@ -0,0 +1,40 @@
+package SearchAlgorithms;
+
+import java.util.*;
+
+public class ListBFS {
+    private Map<Integer, List<Integer>> adjList;
+    public ListBFS(){
+        adjList = new HashMap<>();
+    }
+
+    public void addVertex(int node){
+        adjList.putIfAbsent(node,new ArrayList<>());
+    }
+
+    public void addEdge(int src,int dest){
+        adjList.get(src).add(dest);
+        adjList.get(dest).add(src);
+    }
+
+    //bfs
+    public List<Integer> bfs(int start){
+        List<Integer> result = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        queue.add(start);
+        visited.add(start);
+        while(!queue.isEmpty()){
+            int node= queue.poll();
+            result.add(node);
+            List<Integer> neighbors = adjList.getOrDefault(node,new ArrayList<>());
+            for(int x:neighbors){
+                if(!visited.contains(x)){
+                    queue.add(x);
+                    visited.add(x);
+                }
+            }
+        }
+        return result;
+    }
+}
